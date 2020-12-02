2f5e4e9e001ce77eb90fd8fd604506df5917ee94
keeley <keeley.jun@gmail.com>
2020-12-02 14:07:42 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <bpmn />
+  </div>
+</template>
+
+<script>
+import bpmn from "../components/bpmn";
+
+export default {
+  name: "home",
+  components: {
+    bpmn
+  }
+};
+</script>
